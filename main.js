bd1bbc70785e15726908812eefd84724a081f4f3
sazol <sazolkhan88@gmail.com>
2025-07-11 23:01:15 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// Create a variable called carName, assign the value Volvo to it.
+
+// let ... = "" ; 
+
+let carName= 'Volvo';
+
+
+// On one single line, declare three variables with the following names and values:
+
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+// So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+// 3rd Variable Name And Values Both! 
+
+// let ... = "John";  // write variable name
+// let lastName = "..." //write variable values 
+// let ... = ... ; // write variable name and values both 
+
+let firstName = "John", lastName = "Doe", age = 35;
+
+// 3. Use the correct **assignment operator** that will result in `x` being `50` (same as `x = x * y`).
+
+let x = 5;
+let y = 10;
+let sum = x * y;
+console.log(sum);
